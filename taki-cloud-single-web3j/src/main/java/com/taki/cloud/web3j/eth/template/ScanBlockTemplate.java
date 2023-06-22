13c88d24e8380b5658949ea75ff11a12fccf723c
zhikang peng <1623603371@qq.com>
2023-06-22 13:08:43 +0800

    fix web3j 区块扫描 success

--- taki-cloud-single-web3j/src/main/java/com/taki/cloud/web3j/eth/template/ScanBlockTemplate.java
+++ taki-cloud-single-web3j/src/main/java/com/taki/cloud/web3j/eth/template/ScanBlockTemplate.java
@@ -11,7 +11,7 @@ import java.math.BigInteger;
  * @Date 2023/6/21 18:18
  * @Version 1.0
  */
-public interface ScanBlockTemplate {
+public interface ScanBlockTemplate<T> {
 
     /***
      * @description:  扫描 块 交易
@@ -23,4 +23,14 @@ public interface ScanBlockTemplate {
      * @date: 2023/6/21 18:18
      */
   void scanBlock(Web3j web3j, BigInteger block);
+
+  /***
+   * @description:  通过hash 查询交易
+   * @param web3j
+   * @param  hash
+   * @return  void
+   * @author Long
+   * @date: 2023/6/22 12:56
+   */
+  void hash(Web3j web3j,String hash);
 }
